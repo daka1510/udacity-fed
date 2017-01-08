1675326e5dc96075ba02314eef6df30d9d45613d
daka1510 <daniel.kaulen@gmx.de>
2017-01-08 19:39:22 +0100

    first draft of submission

--- 04-online-resume/dist/js/helper.js
+++ 04-online-resume/dist/js/helper.js
@@ -12,14 +12,14 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="Image of %name%">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a href="%url%">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
@@ -29,17 +29,17 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" alt="%alt%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="%url%">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '<a href="%url%">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
@@ -227,15 +227,7 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
-// Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
 window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
   map.fitBounds(mapBounds);
--- 04-online-resume/dist/js/resumeBuilder.js
+++ 04-online-resume/dist/js/resumeBuilder.js
@@ -1,151 +1,184 @@
-var work = {
-    "jobs" : [{
-        "employer" : "IBM Research & Development GmbH",
-        "title" : "Software Engineer",
-        "location" : "Böblingen",
-        "date" : "2015 - 2016",
-        "description" : "bla blub"
-    },{
-        "employer" : "INFORM GMBH",
-        "title" : "Student",
-        "location" : "Aachen",
-        "date" : "2010 - 2014",
-        "description" : "blub bla"
-    }]
-};
-
-var projects = {
-    "projects" : [{
-        "title" : "Bachelor Thesis",
-        "dates" : "2012",
-        "description" : "An awesome text about my thesis",
-        "images" : ["http://lorempixel.com/197/148/animals/","http://lorempixel.com/197/148/animals/","http://lorempixel.com/197/148/animals/"],
-        "location" : "Aachen"
-    },{
-        "title" : "Master Thesis",
-        "dates" : "2015",
-        "description" : "An awesome text about my thesis",
-        "images" : ["http://lorempixel.com/197/148/animals/", "http://lorempixel.com/197/148/animals/"],
-        "location" : "Aachen",
-    }, {
-        "title" : "Internship",
-        "dates" : "2015",
-        "description" : "An awesome text about my thesis",
-        "images" : ["http://lorempixel.com/197/148/animals/", "http://lorempixel.com/197/148/animals/"],
-        "location" : "Nara, Japan"
-    }]
-};
-
+// resume section data
 
 var bio = {
     "name" : "Daniel Kaulen",
     "role" : "Software Engineer",
     "contacts" : {
+        "mobile" : "+49-151-123456789",
         "email" : "daniel.kaulen@gmx.de",
         "github" : "daka1510",
-        "location" : "Aachen, Germany",
+        "location" : "Aachen, Germany"
     },
     "welcomeMessage" : "lorem ipsum",
-    "skills": ["Java", "HTML", "JavaScript", "CSS"],
+    "skills": ["Java", "Node.JS", "HTML", "JavaScript", "CSS"],
     "bioPic" : "img/me.png"
 };
 
 var education = {
     "schools" : [{
-        "name" : "n",
-        "location" : "San Diego",
-        "degree dates" : "3",
-        "url" : "",
-        "majors" : ["a", "b"]
+        "name" : "RWTH Aachen University",
+        "location" : "Aachen, Germany",
+        "degree" : "Master of Science",
+        "majors" : ["Computer Science"],
+        "dates" : "2012 - 2015",
+        "url" : "http://www.rwth-aachen.de"
+    }, {
+        "name" : "RWTH Aachen University",
+        "location" : "Aachen, Germany",
+        "degree" : "Bachelor of Science",
+        "majors" : ["Communication Science", "Computer Science"],
+        "dates" : "2009 - 2012",
+        "url" : "http://www.rwth-aachen.de"
     }],
     "onlineCourses" : [{
-        "title" : "1",
-        "school" : "3",
-        "dates" : "4",
-        "url" : "4"
+        "title" : "Front-End Web Developer Nanodegree",
+        "school" : "Udacity",
+        "dates" : "2015 - 2016" ,
+        "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
     }]
 }
 
- var formattedName = HTMLheaderName.replace("%data%", bio.name);
- var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
- var picMarkup = HTMLbioPic.replace("%data%", bio.bioPic);
- var welcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
+var work = {
+    "jobs" : [{
+        "employer" : "IBM Research & Development GmbH",
+        "title" : "Software Engineer",
+        "location" : "Böblingen, Germany",
+        "dates" : "in progress",
+        "description" : "Java back-end developer for a cloud-based CMS solution.",
+        "url" : "https://www.ibm.com"
+    },{
+        "employer" : "INFORM GMBH",
+        "title" : "Student Worker",
+        "location" : "Aachen, Germany",
+        "dates" : "2013 - 2014",
+        "description" : "Software development.",
+        "url" : "https://www.inform-software.com"
+    },{
+        "employer" : "INFORM GMBH",
+        "title" : "Student Worker (Test Automation)",
+        "location" : "Aachen, Germany",
+        "dates" : "2010 - 2012",
+        "description" : "Test automation.",
+        "url" : "https://www.inform-software.com"
+    }]
+};
+
+var projects = {
+    "projects" : [{
+        "title" : "Master Thesis, Design Lab at UC San Diego",
+        "dates" : "2015",
+        "description" : "Trauma teams work under extreme time pressure while stabilizing critically injuredpatients and addressing life-threatening injuries. Over the years, several studieshave analyzed different opportunities for technological innovations in this field. Itwas found that there is no optimal position for team-based displays and that largescreens do not adequately address role-based information needs.<br>To compensate for this disadvantage, we analyzed the applicability of head-up displaysduring trauma resuscitations in an exploratory study. A generic prototypingframework for Google Glass was developed to support these efforts. As part of ouruser-centered design process, we conducted interviews with trauma doctors andobserved training sessions that incorporated a human patient simulator. Finally, aworking prototype offering HUD-based support for trauma teams was evaluatedduring two simulated resuscitations.<br>A qualitative analysis showed that the most important application area is the visualizationof vital signs in the field of view (i.e. the doctors do not need to lookaway from the patient to check the vital signs on team-based monitors). Additionally,contextual checklists on individual head-up displays can lead to improvedprotocol adherence and team communication. Our results reduce the design spacefor future projects and serve as basis for upcoming quantitative evaluations of theefficacy of using head-up displays during trauma resuscitation.",
+        "images" : ["img/master-01.jpg","img/master-02.jpg","img/master-03.jpg"],
+        "location" : "San Diego, CA, USA"
+    },{
+        "title" : "Internship, NARA Institute of Science and Technology",
+        "dates" : "2013",
+        "description" : "The development of a widely applicable automatic motion coaching system requires one to address a lot of issues including motion capturing, motion analysis and comparison, error detection as well as error feedback. In order to cope with this complexity, most existing approaches focus on a specific motion sequence or exercise. As a first step towards the development of a more generic system, this paper systematically analyzes different error and feedback types. A prototype of a feedback system that addresses multiple modalities is presented. The system allows to evaluate the applicability of the proposed feedback techniques for arbitrary types of motions in a next step.",
+        "images" : ["img/naist-01.jpg", "img/naist-02.jpg"],
+        "location" : "Nara, Japan"
+    },{
+        "title" : "Bachelor Thesis, RWTH Aachen",
+        "dates" : "2012",
+        "description" : "This thesis analyzes the requirements of users of different generations with regard to future e-banking systems. An empirical study was conducted to verify six previously formulated hypotheses. The results show that the acceptance of such systems highly depends on factors such as age, gender and general technical affinity. It turned out that most of the older people are not yet willing to use such systems at all – independent of specific system characteristics. Additionally, it is more likely that men will use smart banking systems than women and that technical affinity has a positive influence on the general usage intention.",
+        "images" : ["img/bachelor-01.jpg", "img/bachelor-02.jpg"],
+        "location" : "Aachen, Germany",
+    }]
+};
+
+// display functions
 
 bio.display = function() {
-    for (var key in bio.contacts) {
-      if (bio.contacts.hasOwnProperty(key)) {
-        $("#topContacts").append(HTMLcontactGeneric.replace("%contact%", key).replace("%data%", bio.contacts[key]))
+    // add name and role
+    $("#header").prepend(HTMLheaderRole.replace("%data%", this.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%", this.name));
+
+    // show available contact information
+    for (var key in this.contacts) {
+      if (this.contacts.hasOwnProperty(key)) {
+        var contactMarkup = HTMLcontactGeneric.replace("%contact%", key).replace("%data%", this.contacts[key]);
+        $("#topContacts").append(contactMarkup);
+        $("#footerContacts").append(contactMarkup);
       }
     }
-}
-bio.display();
 
+    // add picture along with a welcome message
+    $("#header").append(HTMLbioPic.replace("%data%", this.bioPic).replace("%name%", this.name));
+    $("#header").append(HTMLwelcomeMsg.replace("%data%", this.welcomeMessage));
 
- $("#header").append(picMarkup);
- $("#header").append(welcomeMessage);
-
- if(bio.skills.length > 0){
-    $("#header").append(HTMLskillsStart);
-    for (idx in bio.skills) {
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[idx]);
-        $("#skills").append(formattedSkill);
+    // add skills
+    if(this.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        this.skills.forEach(function(value) {
+            $("#skills").append(HTMLskills.replace("%data%", value));
+        });
     }
- }
+};
 
- function locationizer(work_obj){
-    var result = [];
-    for(idx in work_obj.jobs){
-        result.push(work_obj.jobs[idx].location)
+education.display = function() {
+    // display schools
+    this.schools.forEach(function(school) {
+        $("#education").append(HTMLschoolStart);
+        var formattedName = HTMLschoolName.replace("%data%", school.name).replace("%url%", school.url);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", school.majors);
+
+        $(".education-entry:last").append(formattedName
+            .concat(formattedDegree)
+            .concat(formattedDates)
+            .concat(formattedLocation)
+            .concat(formattedMajor));
+    });
+
+    // display online courses
+    if(this.onlineCourses.length > 0){
+        $("#education").append(HTMLonlineClasses);
+        this.onlineCourses.forEach(function(course) {
+            $("#education").append(HTMLschoolStart);
+            var formattedTitle = HTMLonlineTitle.replace("%data%", course.title).replace("%url%", course.url);
+            var formattedSchool = HTMLonlineSchool.replace("%data%", course.school)
+            var formattedDates = HTMLonlineDates.replace("%data%", course.dates)
+            $(".education-entry:last").append(formattedTitle
+                .concat(formattedSchool)
+                .concat(formattedDates)).append("<br>");
+        });
     }
-    return result;
- }
-
- function displayWork(){
-     if(work.jobs.length > 0) {
-        for (idx in work.jobs) {
-            $("#workExperience").append(HTMLworkStart);
-            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[idx].employer);
-            var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[idx].title);
-            var formattedJobDates = HTMLworkDates.replace("%data%", work.jobs[idx].date);
-            var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[idx].description);
-
-            $(".work-entry:last").append(formattedEmployer.concat(formattedJobTitle).concat(formattedJobDates).concat(formattedJobDescription));
-        }
-     }
- };
- displayWork();
-
-$(document).click(function(loc) {
-  logClicks(loc.pageX, loc.pageY)
-});
-
-function inName(name) {
-    // split into first and last name
-    console.log(name);
-    var fragments = name.split(" ");
-    var firstName = fragments[0];
-    var lastName = fragments[1];
-    return firstName.charAt(0).toUpperCase() + firstName.slice(1) + " " + lastName.toUpperCase();
 }
 
-$("#main").append(internationalizeButton);
+work.display = function() {
+    this.jobs.forEach(function(job) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer).replace("%url%", job.url);
+        var formattedJobTitle = HTMLworkTitle.replace("%data%", job.title);
+        var formattedJobDates = HTMLworkDates.replace("%data%", job.dates);
+        var formattedJobDescription = HTMLworkDescription.replace("%data%", job.description);
+        $(".work-entry:last").append(formattedEmployer
+            .concat(formattedJobTitle)
+            .concat(formattedJobDates)
+            .concat(formattedJobDescription));
+    });
+};
 
 projects.display = function() {
-    for(idx in projects.projects) {
+    this.projects.forEach(function(project) {
+        // compute markup
         $("#projects").append(HTMLprojectStart);
-        var projectData = "";
-        projectData = HTMLprojectTitle.replace("%data%", projects.projects[idx].title)
-                        + HTMLprojectDates.replace("%data%", projects.projects[idx].dates)
-                        + HTMLprojectDescription.replace("%data%", projects.projects[idx].description);
-        for(imageId in projects.projects[idx].images){
-            projectData = projectData + HTMLprojectImage.replace("%data%", projects.projects[idx].images[imageId]);
-        }
-        $(".project-entry:last").append(projectData);
-    }
-}
+        var projectData = HTMLprojectTitle.replace("%data%", project.title)
+                        + HTMLprojectDates.replace("%data%", project.dates)
+                        + HTMLprojectDescription.replace("%data%", project.description);
 
+        project.images.forEach(function(imageUrl) {
+            projectData = projectData + HTMLprojectImage.replace("%data%", imageUrl).replace("%alt%", "Image of " + project.title);
+        });
+        // add to DOM
+        $(".project-entry:last").append(projectData);
+    });
+}
 projects.display();
+education.display();
+bio.display();
+work.display();
+
 
 $("#mapDiv").append(googleMap);
