d899b36144e59d02e1e5141ed43ad5b8e84bcf82
daka1510 <daniel.kaulen@gmx.de>
2017-01-21 21:22:54 +0100

    fixed gulp setup

--- 05-classic-arcade-game-clone/gulpfile.js
+++ 05-classic-arcade-game-clone/gulpfile.js
@@ -1,30 +1,12 @@
 // import gulp
 var gulp = require('gulp'),
-    connect = require('gulp-connect'),
-    jslint = require('gulp-jslint'),
-    csslint = require('gulp-csslint');
+    connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
-    root: 'dist',
+    root: 'app',
     livereload: true
   });
 });
 
-
-gulp.task('verifyJS', function () {
-    return gulp.src(['dist/js/resumeBuilder.js'])
-            .pipe(jslint(   ))
-            .pipe(jslint.reporter('default'));
-});
-
-
-gulp.task('verifyCSS', function() {
-  gulp.src('dist/css/*.css')
-    .pipe(csslint())
-    .pipe(csslint.formatter()) // display errors
-    .pipe(csslint.formatter('fail')); // fail on error
-});
-
-gulp.task('verify', ['verifyCSS', 'verifyJS']);
 gulp.task('default', ['connect']);
