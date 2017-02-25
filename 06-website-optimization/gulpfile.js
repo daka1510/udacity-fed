0a2d4251b57fb1a555cf51f4e40a21fafc6cb59b
daka1510 <daniel.kaulen@gmx.de>
2017-02-25 11:44:55 +0100

    smaller imges

--- 06-website-optimization/gulpfile.js
+++ 06-website-optimization/gulpfile.js
@@ -1,13 +1,51 @@
-// import gulp
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+// variables
+var gulp      = require('gulp');
+var ngrok     = require('ngrok');
+var psi       = require('psi');
+var sequence  = require('run-sequence');
+var site      = '';
+var portVal   = 3020;
+var browserSync = require('browser-sync');
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./app"
-        }
-    });
+gulp.task('ngrok-url', function(cb) {
+  return ngrok.connect(portVal, function (err, url) {
+    site = url;
+    console.log('serving your tunnel from: ' + site);
+    cb();
+  });
 });
 
-gulp.task('default', ['browser-sync']);
+gulp.task('psi-desktop', function (cb) {
+  psi(site, {
+    nokey: 'true',
+    strategy: 'desktop'
+  }, cb);
+});
+
+gulp.task('psi-mobile', function (cb) {
+  psi(site, {
+    nokey: 'true',
+    strategy: 'mobile'
+  }, cb);
+});
+
+gulp.task('test-psi', function (cb) {
+  return sequence(
+    'serve',
+    'ngrok-url',
+    'psi-desktop',
+    'psi-mobile',
+    cb
+  );
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve', function() {
+  browserSync({
+    port: portVal,
+    notify: false,
+    server: 'dist',
+    baseDir: 'dist',
+    open: false,
+  });
+});
