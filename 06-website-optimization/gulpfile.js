18014da8736b3de6a3c3ed7bde059d879dfb52d3
daka1510 <daniel.kaulen@gmx.de>
2017-02-25 12:18:14 +0100

    use app

--- 06-website-optimization/gulpfile.js
+++ 06-website-optimization/gulpfile.js
@@ -1,13 +1,12 @@
 var gulp        = require('gulp');
 var port        = 3020;
 var browserSync = require('browser-sync');
-
 gulp.task('serve', function() {
   browserSync({
     port: port,
     notify: false,
-    server: 'dist',
-    baseDir: 'dist',
+    server: 'app',
+    baseDir: 'app',
     open: false,
   });
 });
