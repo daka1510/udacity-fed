fbe040fe0ab75e75f8a4f437760dc0ec527b3719
daka1510 <daniel.kaulen@gmx.de>
2017-02-25 12:15:55 +0100

    cleanup

--- 06-website-optimization/gulpfile.js
+++ 06-website-optimization/gulpfile.js
@@ -1,51 +1,13 @@
-// variables
-var gulp      = require('gulp');
-var ngrok     = require('ngrok');
-var psi       = require('psi');
-var sequence  = require('run-sequence');
-var site      = '';
-var portVal   = 3020;
+var gulp        = require('gulp');
+var port        = 3020;
 var browserSync = require('browser-sync');
 
-gulp.task('ngrok-url', function(cb) {
-  return ngrok.connect(portVal, function (err, url) {
-    site = url;
-    console.log('serving your tunnel from: ' + site);
-    cb();
-  });
-});
-
-gulp.task('psi-desktop', function (cb) {
-  psi(site, {
-    nokey: 'true',
-    strategy: 'desktop'
-  }, cb);
-});
-
-gulp.task('psi-mobile', function (cb) {
-  psi(site, {
-    nokey: 'true',
-    strategy: 'mobile'
-  }, cb);
-});
-
-gulp.task('test-psi', function (cb) {
-  return sequence(
-    'serve',
-    'ngrok-url',
-    'psi-desktop',
-    'psi-mobile',
-    cb
-  );
-});
-
-// Build and serve the output from the dist build
 gulp.task('serve', function() {
   browserSync({
-    port: portVal,
+    port: port,
     notify: false,
-    server: 'app',
-    baseDir: 'app',
+    server: 'dist',
+    baseDir: 'dist',
     open: false,
   });
 });
